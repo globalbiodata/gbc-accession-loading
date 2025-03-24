d5699a7b7a901c7a6fdcb5d0030e88e0dc7a1645
Carla Cummins <carlac@ebi.ac.uk>
2025-03-24 09:55:08 +0000

    Pull files from ePMC bulk downloads

--- bin/query_epmc.py
+++ bin/query_epmc.py
@@ -13,47 +13,17 @@ import pymysql
 import sqlalchemy as db
 
 parser = argparse.ArgumentParser(description='Query EuropePMC for accession data.')
-# parser.add_argument('--cursor-file', type=str, help='File to read/write cursor mark', required=True)
+parser.add_argument('--csv-file', type=str, help='CSV file of text mined accessions', required=True)
+parser.add_argument('--resource', type=str, help='Resource name', required=True)
 parser.add_argument('--accession-types', type=str, help='Path to JSON file with accession types', required=True)
 parser.add_argument('--outdir', type=str, help='Output directory for results', required=True)
-
-parser.add_argument('--db', type=str, help='Database to use (format: instance_name/db_name)', required=True)
-parser.add_argument('--dbcreds', type=str, help='Path to JSON file with SQL credentials')
-parser.add_argument('--sqluser', type=str, help='SQL user', default=os.environ.get("CLOUD_SQL_USER"))
-parser.add_argument('--sqlpass', type=str, help='SQL password', default=os.environ.get("CLOUD_SQL_PASSWORD"))
-
-parser.add_argument('--page-size', type=int, default=1000, help='Number of results per page')
-parser.add_argument('--limit', type=int, default=0, help='Limit the number of results')
+parser.add_argument('--batch-size', type=int, default=1000, help='Number of results per page')
 
 args = parser.parse_args()
 limit = args.limit if args.limit > 0 else None
 
 if not os.path.exists(args.outdir):
     os.makedirs(args.outdir)
-
-# setup SQL connection
-sqluser, sqlpass = None, None
-if args.dbcreds:
-    creds = json.load(open(args.dbcreds, 'r'))
-    sqluser, sqlpass = creds.get('user'), creds.get('pass')
-elif args.sqluser and args.sqlpass:
-    sqluser, sqlpass = args.sqluser, args.sqlpass
-else:
-    sys.exit("Error: No SQL credentials provided")
-
-gcp_connector = Connector()
-instance, db_name = args.db.split('/')
-def getcloudconn() -> pymysql.connections.Connection:
-    conn: pymysql.connections.Connection = gcp_connector.connect(
-        instance, "pymysql",
-        user=sqluser,
-        password=sqlpass,
-        db=db_name
-    )
-    return conn
-
-cloud_engine = db.create_engine("mysql+pymysql://", creator=getcloudconn, pool_recycle=60 * 5, pool_pre_ping=True)
-cloud_conn = cloud_engine.connect()
 
 max_retries = 10
 def query_europepmc(endpoint, request_params, retry_count=0, graceful_exit=False):
@@ -79,7 +49,7 @@ def query_europepmc(endpoint, request_params, retry_count=0, graceful_exit=False
     return data
 
 # hash directory for results files (to avoid overpopulating a single directory)
-def generate_json_file(c):
+def hashed_json_filename(c):
     cstr = str(c).zfill(7)
     hashdir = '/'.join(list(cstr)[::-1][:4])
     outdir = f"{args.outdir}/{hashdir}"
@@ -95,20 +65,12 @@ epmc_fields = [
     'keywordList', 'meshHeadingList', 'citedByCount', 'hasTMAccessionNumbers'
 ]
 
-db_cursors = cloud_conn.execute(db.text("SELECT cursor_mark, cursor_id FROM tmp_cursor_tracking ORDER BY time DESC LIMIT 1")).fetchone()
-if db_cursors:
-    cursor, c = db_cursors
-    c = int(c)
-else:
-    cursor, c = None, 1
-
 epmc_base_url = "https://www.ebi.ac.uk/europepmc/webservices/rest"
 more_data = True
 while more_data:
     search_params = {
         'query': acc_query, 'resultType': 'core',
-        'format': 'json', 'pageSize': args.page_size,
-        'cursorMark': cursor
+        'format': 'json', 'pageSize': args.page_size
     }
 
     # if error, exit gracefully : this allows us to resume from where we left off in the
@@ -134,6 +96,3 @@ while more_data:
     cursor = data.get('nextCursorMark')
     if not cursor or limit <= 0:
         more_data = False
-    else:
-        cloud_conn.execute(db.text(f"INSERT INTO tmp_cursor_tracking (cursor_mark, cursor_id) VALUES ('{cursor}', {c})"))
-        cloud_conn.commit()
---
+++ modules/downloadTMFiles.nf
@@ -0,0 +1,17 @@
+process DOWNLOAD_TM_FILES {
+    label 'process_tiny'
+    debug true
+
+    input:
+    val(download_url)
+
+    output:
+    path("text_mined_csvs/*.csv"), emit: accession_csvs
+
+    script:
+    outdir = "text_mined_csvs"
+    """
+    wget -r -l 1 -A "*.csv" -nd -e robots=off ${download_url} \
+    -P ${outdir}
+    """
+}
